da9f06686ae2bd8cbefe0300b1584cb1fafe3a4d
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-01-31 00:24:47 +0200

    update main.go, fixed spelling error

--- internal/server/main.go
+++ internal/server/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen("8080")
 }
