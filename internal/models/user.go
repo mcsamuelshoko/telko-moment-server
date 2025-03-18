8a0890c44effc4d37b2e0c42dcda5ca0ddea93ea
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-18 13:30:31 +0200

    added more user properties

--- internal/models/user.go
+++ internal/models/user.go
@@ -4,17 +4,19 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	Id                 primitive.ObjectID //(unique, primary key)
-	firstName          string
-	lastName           string
-	email              string //(unique)
-	phoneNumber        string //(optional)
-	userType           string //(regular, enterprise)
-	profilePicture     string //(URL to image)
-	status             string //(online, offline, busy, etc.)
-	bio                string
-	languagePreference string
-	timezone           string
-	country            string
-	createdAt          primitive.DateTime
-	updatedAt          primitive.DateTime
+	FirstName          string
+	LastName           string
+	Username           string
+	Password           string
+	Email              string //(unique)
+	PhoneNumber        string //(optional)
+	UserType           string //(regular, enterprise)
+	ProfilePicture     string //(URL to image)
+	Status             string //(online, offline, busy, etc.)
+	Bio                string
+	LanguagePreference string
+	Timezone           string
+	Country            string
+	CreatedAt          primitive.DateTime
+	UpdatedAt          primitive.DateTime
 }
