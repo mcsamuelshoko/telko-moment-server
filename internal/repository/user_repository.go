75faed8e8284cb5e3f557db9ad0a0b9ba7c87749
Mc Samuel Shoko <26929659-mcsamuelshoko@users.noreply.gitlab.com>
2025-04-08 14:09:49 +0200

    update: added getByPhoneNumber method

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -5,12 +5,13 @@ import (
 	"github.com/mcsamuelshoko/telko-moment-server/internal/models"
 )
 
-type UserRepository interface {
-	Create(ctx context.Context, user *models.User) error
+type IUserRepository interface {
+	Create(ctx context.Context, user *models.User) (*models.User, error)
 	GetByID(ctx context.Context, id string) (*models.User, error)
 	GetByEmail(ctx context.Context, email string) (*models.User, error)
 	GetByUsername(ctx context.Context, username string) (*models.User, error)
+	GetByPhoneNumber(ctx context.Context, phoneNumber string) (*models.User, error)
 	List(ctx context.Context, page, limit int) ([]models.User, error)
-	Update(ctx context.Context, user *models.User) error
+	Update(ctx context.Context, user *models.User) (*models.User, error)
 	Delete(ctx context.Context, id string) error
 }
