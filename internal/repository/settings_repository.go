f455d22a6aff4a910c05919683f45206d07ae855
Mc Samuel Shoko <26929659-mcsamuelshoko@users.noreply.gitlab.com>
2025-04-11 07:52:11 +0200

    update: refactored create method return values

--- internal/repository/settings_repository.go
+++ internal/repository/settings_repository.go
@@ -6,7 +6,7 @@ import (
 )
 
 type SettingsRepository interface {
-	Create(ctx context.Context, settings *models.Settings) error
+	Create(ctx context.Context, settings *models.Settings) (*models.Settings, error)
 	GetByID(ctx context.Context, id string) (*models.Settings, error)
 	GetByUserID(ctx context.Context, userId string) (*models.Settings, error)
 	List(ctx context.Context, page, limit int) ([]models.Settings, error)
