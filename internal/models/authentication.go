b1395ed31fe9e0eb032b50e81dfd8d109b3f7a1b
Mc Samuel Shoko <26929659-mcsamuelshoko@users.noreply.gitlab.com>
2025-04-11 07:36:26 +0200

    update: added hash fields & default initializers

--- internal/models/authentication.go
+++ internal/models/authentication.go
@@ -2,7 +2,7 @@ package models
 
 import (
 	"context"
-	"github.com/mcsamuelshoko/telko-moment-server/pkg/utils"
+	"github.com/mcsamuelshoko/telko-moment-server/pkg/services"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -12,15 +12,16 @@ import (
 
 // Authentication represents the stored authentication information in MongoDB
 type Authentication struct {
-	ID           primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	UserID       primitive.ObjectID `json:"userId,omitempty" bson:"userId,omitempty"`
-	RefreshToken string             `json:"refreshToken,omitempty" bson:"refreshToken,omitempty"`
-	IsActive     bool               `json:"isActive" bson:"isActive"`
-	ExpiresAt    time.Time          `json:"expiresAt" bson:"expiresAt"`
-	AuthProvider string             `json:"authProvider" bson:"authProvider"`
-	LastLogin    time.Time          `json:"lastLogin,omitempty" bson:"lastLogin,omitempty"`
-	CreatedAt    time.Time          `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
-	UpdatedAt    time.Time          `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
+	ID               primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	UserID           primitive.ObjectID `json:"userId,omitempty" bson:"userId,omitempty"`
+	RefreshToken     string             `json:"refreshToken,omitempty" bson:"refreshToken,omitempty"`
+	RefreshTokenHash string             `json:"-" bson:"refreshTokenHash,omitempty"`
+	IsActive         bool               `json:"isActive" bson:"isActive"`
+	ExpiresAt        time.Time          `json:"expiresAt" bson:"expiresAt"`
+	AuthProvider     string             `json:"authProvider" bson:"authProvider"`
+	LastLogin        time.Time          `json:"lastLogin,omitempty" bson:"lastLogin,omitempty"`
+	CreatedAt        time.Time          `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
+	UpdatedAt        time.Time          `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
 }
 
 // CreateUniqueIndexes creates unique indexes for userId and refreshToken
@@ -32,20 +33,33 @@ func (a *Authentication) CreateUniqueIndexes(db *mongo.Database) error {
 	}
 
 	// Create unique index for refreshToken
-	refreshTokenIndex := mongo.IndexModel{
-		Keys:    bson.D{{"refreshToken", 1}},
-		Options: options.Index().SetUnique(true).SetName("unique_refresh_token"),
+	refreshTokenHashIndex := mongo.IndexModel{
+		Keys:    bson.D{{"refreshTokenHash", 1}},
+		Options: options.Index().SetUnique(true).SetName("unique_refresh_token_hash"),
 	}
 
 	// Create indexes
 	_, err := db.Collection("authentications").Indexes().
-		CreateMany(context.Background(), []mongo.IndexModel{userIdIndex, refreshTokenIndex})
+		CreateMany(context.Background(), []mongo.IndexModel{userIdIndex, refreshTokenHashIndex})
 
 	return err
 }
 
+// HashFields It is called before EncryptFields so that it will not hash already transformed data, it Hashes sensitive fields for easier search,
+// than to search their encrypted variants which are non-deterministic in their encryption
+func (a *Authentication) HashFields(keyHashSvc services.ISearchKeyService) error {
+	if a.RefreshToken != "" {
+		hashed, err := keyHashSvc.GenerateSearchKey(a.RefreshToken)
+		if err != nil {
+			return err
+		}
+		a.RefreshTokenHash = hashed
+	}
+	return nil
+}
+
 // EncryptFields Encrypt sensitive fields before saving
-func (a *Authentication) EncryptFields(encSvc utils.IEncryptionService) error {
+func (a *Authentication) EncryptFields(encSvc services.IEncryptionService) error {
 	if a.RefreshToken != "" {
 		encrypted, err := encSvc.Encrypt(a.RefreshToken)
 		if err != nil {
@@ -57,7 +71,7 @@ func (a *Authentication) EncryptFields(encSvc utils.IEncryptionService) error {
 }
 
 // DecryptFields Decrypt sensitive fields after retrieval
-func (a *Authentication) DecryptFields(encSvc utils.IEncryptionService) error {
+func (a *Authentication) DecryptFields(encSvc services.IEncryptionService) error {
 	if a.RefreshToken != "" {
 		decrypted, err := encSvc.Decrypt(a.RefreshToken)
 		if err != nil {
@@ -68,8 +82,25 @@ func (a *Authentication) DecryptFields(encSvc utils.IEncryptionService) error {
 	return nil
 }
 
-// LoginRequest represents the data needed for a login attempt
-type LoginRequest struct {
+// :::: DEFAULTS FUNCTION(S)
+
+// GetAuthenticationDefaults Get user defaults from request headers
+func GetAuthenticationDefaults() *Authentication {
+	auth := &Authentication{
+		LastLogin:    time.Now(),
+		AuthProvider: "JWT",
+		IsActive:     true,
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
+	}
+
+	return auth
+}
+
+// :::: REQUEST RESPONSE
+
+// LoginRequestUsername represents the data needed for a login attempt
+type LoginRequestUsername struct {
 	Username string `json:"username" validate:"required,username"`
 	Password string `json:"password" validate:"required"`
 }
@@ -96,4 +127,12 @@ type RegisterRequest struct {
 type RegisterRequestEmail struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
+}
+
+type UpdateTokenRequest struct {
+	RefreshToken string `json:"refreshToken,omitempty"`
+}
+
+type LogoutRequest struct {
+	RefreshToken string `json:"refreshToken,omitempty"`
 }
