0febcb552365175710f1715aa5a840e10722f9dd
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-04-21 09:06:01 +0200

    feat: created auth_context_middleware to get user-object into context

---
+++ internal/handlers/middleware/auth_context_middleware.go
@@ -0,0 +1,71 @@
+package middleware
+
+import (
+	"errors"
+	"github.com/gofiber/fiber/v2"
+	"github.com/mcsamuelshoko/telko-moment-server/internal/repository"
+	"github.com/mcsamuelshoko/telko-moment-server/pkg/utils"
+	"github.com/rs/zerolog"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// ContextKey is a custom type for context keys
+type ContextKey string
+
+const UserObjectContextKey ContextKey = "userObject"
+const UserIDContextKey ContextKey = "userID" // Use primitive.ObjectID
+
+type AuthContextMiddleware struct {
+	userRepo repository.IUserRepository
+	logger   *zerolog.Logger // logger
+}
+
+func NewAuthContextMiddleware(log *zerolog.Logger, userRepo repository.IUserRepository) *AuthContextMiddleware {
+	return &AuthContextMiddleware{
+		userRepo: userRepo,
+		logger:   log,
+	}
+}
+
+func (acm *AuthContextMiddleware) AddUserContext() fiber.Handler {
+	//return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return func(c *fiber.Ctx) error {
+		userIDStr, ok := c.Context().Value(UserIDStrContextKey).(string)
+		if !ok || userIDStr == "" {
+			//http.Error(w, "Unauthorized: Missing user identifier", http.StatusUnauthorized)
+			acm.logger.Error().Msg("Invalid user id from context")
+			return c.Status(fiber.StatusUnauthorized).JSON(utils.ErrorResponse("Missing user identifier"))
+		}
+
+		userID, err := primitive.ObjectIDFromHex(userIDStr)
+		if err != nil {
+			//http.Error(w, "Unauthorized: Invalid user identifier", http.StatusUnauthorized)
+			acm.logger.Error().Err(err).Msg("Invalid user id hex() string value from context")
+			return c.Status(fiber.StatusUnauthorized).JSON(utils.ErrorResponse("Invalid user id hex string"))
+		}
+
+		// Fetch the user object
+		user, err := acm.userRepo.GetByID(c.Context(), userIDStr)
+		if err != nil {
+			if errors.Is(err, err) { // Use specific errors
+				//http.Error(w, "Unauthorized: User not found", http.StatusUnauthorized)
+				acm.logger.Error().Err(err).Msg("Unauthorized: User not found")
+				return c.Status(fiber.StatusUnauthorized).JSON(utils.ErrorResponse("User not found"))
+			} else {
+				// Log the actual error
+				//http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				acm.logger.Error().Err(err).Msg("Error while getting user")
+				return c.Status(fiber.StatusInternalServerError).JSON(utils.ErrorResponse("Error while getting user"))
+			}
+		}
+
+		// Add user object and validated userID to context
+		//ctx := context.WithValue(r.Context(), UserObjectContextKey, user)
+		//ctx = context.WithValue(ctx, UserIDContextKey, user.ID) // Add the ObjectID
+		c.Locals(UserObjectContextKey, user)
+		c.Locals(UserIDContextKey, userID)
+
+		//next.ServeHTTP(w, r.WithContext(ctx))
+		return c.Next()
+	}
+}
