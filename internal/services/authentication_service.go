ecf0781cc4023fc3588dbd3fd9e088e3c2ab804e
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-04-23 12:12:52 +0200

    chore: added logging interface for easier identification/tracing in logs

--- internal/services/authentication_service.go
+++ internal/services/authentication_service.go
@@ -15,48 +15,58 @@ type IAuthenticationService interface {
 }
 
 type AuthenticationService struct {
-	log  *zerolog.Logger
-	repo repository.IAuthenticationRepository
+	iName string
+	log   *zerolog.Logger
+	repo  repository.IAuthenticationRepository
 }
 
 func NewAuthenticationService(log *zerolog.Logger, repo repository.IAuthenticationRepository) IAuthenticationService {
 	return &AuthenticationService{
-		log:  log,
-		repo: repo,
+		iName: "AuthenticationService",
+		log:   log,
+		repo:  repo,
 	}
 }
 
 func (a AuthenticationService) SaveRefreshToken(ctx context.Context, userID string, refreshToken string, tokenDuration time.Duration) error {
+	const kName = "SaveRefreshToken"
+
 	err := a.repo.SaveRefreshToken(ctx, userID, refreshToken, tokenDuration)
 	if err != nil {
-		a.log.Error().Err(err).Str("userID", userID).Msg("Failed to save refresh token")
+		a.log.Error().Err(err).Interface(kName, a.iName).Msg("Failed to save refresh token")
 		return err
 	}
 	return nil
 }
 
 func (a AuthenticationService) GetUserIDFromRefreshToken(ctx context.Context, refreshToken string) (string, error) {
+	const kName = "GetUserIDFromRefreshToken"
+
 	userID, err := a.repo.GetUserIDFromRefreshToken(ctx, refreshToken)
 	if err != nil {
-		a.log.Error().Err(err).Str("refreshToken", refreshToken).Msg("Failed to get user ID from refresh token")
+		a.log.Error().Err(err).Interface(kName, a.iName).Msg("Failed to get user ID from refresh token")
 		return "", err
 	}
 	return userID, nil
 }
 
 func (a AuthenticationService) UpdateUserRefreshToken(ctx context.Context, userID string, refreshToken string, tokenDuration time.Duration) error {
+	const kName = "UpdateUserRefreshToken"
+
 	err := a.repo.SaveRefreshToken(ctx, userID, refreshToken, tokenDuration)
 	if err != nil {
-		a.log.Error().Err(err).Str("userID", userID).Msg("Failed to update refresh token")
+		a.log.Error().Err(err).Interface(kName, a.iName).Msg("Failed to update refresh token")
 		return err
 	}
 	return nil
 }
 
 func (a AuthenticationService) DeleteRefreshToken(ctx context.Context, refreshToken string) error {
+	const kName = "DeleteRefreshToken"
+
 	err := a.repo.DeleteRefreshToken(ctx, refreshToken)
 	if err != nil {
-		a.log.Error().Err(err).Str("refreshToken", refreshToken).Msg("Failed to delete refresh token")
+		a.log.Error().Err(err).Interface(kName, a.iName).Msg("Failed to delete refresh token")
 		return err
 	}
 	return nil
