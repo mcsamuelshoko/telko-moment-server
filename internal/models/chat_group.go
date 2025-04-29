f9ecfbf5cfc4b74644a773d2b68fd3eb99554c34
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-04-29 23:47:05 +0200

    update: added more properties,
    And renamed Ids to all-caps as an abbreviation to follow golang conventions

--- internal/models/chat_group.go
+++ internal/models/chat_group.go
@@ -3,11 +3,13 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type ChatGroup struct {
-	Id          primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
+	ID          primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
 	Name        string               `json:"name" bson:"name"`
 	Type        string               `json:"type" bson:"type"`
+	ChatID      primitive.ObjectID   `json:"chatId" bson:"chatId"`
+	CreatorID   primitive.ObjectID   `json:"creatorId,omitempty" bson:"creatorId,omitempty"`
 	Members     []primitive.ObjectID `json:"members" bson:"members"`
-	AdminIds    []primitive.ObjectID `json:"adminIds" bson:"adminIds"`
+	AdminIDs    []primitive.ObjectID `json:"adminIds" bson:"adminIds"`
 	Description string               `json:"description,omitempty" bson:"description,omitempty"`
 	ProfileUrl  string               `json:"profileUrl,omitempty" bson:"profileUrl,omitempty"`
 	CreatedAt   primitive.DateTime   `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
