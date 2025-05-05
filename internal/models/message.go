d7452ef76b04677b5a623772e1f97ae1c5b50019
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-05-05 23:32:38 +0200

    update: followed convention for goland IDs and added createdAt and UpdatedAt properties

--- internal/models/message.go
+++ internal/models/message.go
@@ -1,11 +1,14 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
 
 type Message struct {
-	Id                 primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
-	ChatId             primitive.ObjectID   `json:"chatId" bson:"chatId"`
-	SenderId           primitive.ObjectID   `json:"senderId" bson:"senderId"`
+	ID                 primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
+	ChatID             primitive.ObjectID   `json:"chatId" bson:"chatId"`
+	SenderID           primitive.ObjectID   `json:"senderId" bson:"senderId"`
 	MessageType        string               `json:"messageType" bson:"messageType"`
 	Content            string               `json:"content,omitempty" bson:"content,omitempty"`
 	MediaUrls          []string             `json:"mediaUrls,omitempty" bson:"mediaUrls,omitempty"`
@@ -14,5 +17,7 @@ type Message struct {
 	EditedMessage      bool                 `json:"editedMessage" bson:"editedMessage"`
 	Status             string               `json:"status" bson:"status"`
 	Mentions           []primitive.ObjectID `json:"mentions,omitempty" bson:"mentions,omitempty"`
-	RepliedToMessageId primitive.ObjectID   `json:"repliedToMessageId,omitempty" bson:"repliedToMessageId,omitempty"`
+	RepliedToMessageID primitive.ObjectID   `json:"repliedToMessageId,omitempty" bson:"repliedToMessageId,omitempty"`
+	CreatedAt          time.Time            `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
+	UpdatedAt          time.Time            `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
 }
