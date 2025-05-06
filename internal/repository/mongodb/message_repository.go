220e7c0dd14f963a733767e8c6abcb395492de64
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-05-06 05:48:40 +0200

    update: adder logger via injection instead

--- internal/repository/mongodb/message_repository.go
+++ internal/repository/mongodb/message_repository.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"github.com/mcsamuelshoko/telko-moment-server/internal/models"
 	"github.com/mcsamuelshoko/telko-moment-server/internal/repository"
-	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -14,12 +14,14 @@ import (
 type messageRepository struct {
 	Collection *mongo.Collection
 	iName      string
+	logger     *zerolog.Logger
 }
 
-func NewMessageRepository(db *mongo.Database) repository.MessageRepository {
+func NewMessageRepository(log *zerolog.Logger, db *mongo.Database) repository.MessageRepository {
 	return &messageRepository{
 		iName:      "MessageRepository",
 		Collection: db.Collection("messages"),
+		logger:     log,
 	}
 }
 
@@ -28,7 +30,7 @@ func (m messageRepository) Create(ctx context.Context, message *models.Message) 
 
 	res, err := m.Collection.InsertOne(ctx, message)
 	if err != nil {
-		log.Error().Interface(kName, m.iName).Err(err).Msg("Error inserting message")
+		m.logger.Error().Interface(kName, m.iName).Err(err).Msg("Error inserting message")
 		return nil, err
 	}
 	message.ID = res.InsertedID.(primitive.ObjectID)
@@ -41,14 +43,14 @@ func (m messageRepository) GetByID(ctx context.Context, id string) (*models.Mess
 	// message ID to search for
 	messageID, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
-		log.Error().Interface(kName, m.iName).Err(err).Msg("failed to convert Message-GetById-id to object id")
-		log.Debug().Interface(kName, m.iName).Err(err).Msg("failed to convert message-GetById id:" + id)
+		m.logger.Error().Interface(kName, m.iName).Err(err).Msg("failed to convert Message-GetById-id to object id")
+		m.logger.Debug().Interface(kName, m.iName).Err(err).Msg("failed to convert message-GetById id:" + id)
 		return nil, err
 	}
 	message := &models.Message{}
 	err = m.Collection.FindOne(ctx, bson.M{"_id": messageID}).Decode(message)
 	if err != nil {
-		log.Error().Interface(kName, m.iName).Err(err).Msg("Error finding message in GetByID")
+		m.logger.Error().Interface(kName, m.iName).Err(err).Msg("Error finding message in GetByID")
 		return nil, err
 	}
 	return message, nil
@@ -61,14 +63,14 @@ func (m messageRepository) GetByChatID(ctx context.Context, chatId string) (*mod
 	// chat ID to search for
 	chatID, err := primitive.ObjectIDFromHex(chatId)
 	if err != nil {
-		log.Error().Interface(kName, m.iName).Err(err).Msg("failed to convert Message-GetByChatId-id to object id")
-		log.Debug().Interface(kName, m.iName).Err(err).Msg("failed to convert message-GetByChat id:" + chatId)
+		m.logger.Error().Interface(kName, m.iName).Err(err).Msg("failed to convert Message-GetByChatId-id to object id")
+		m.logger.Debug().Interface(kName, m.iName).Err(err).Msg("failed to convert message-GetByChat id:" + chatId)
 		return nil, err
 	}
 	message := &models.Message{}
 	err = m.Collection.FindOne(ctx, bson.M{"chatId": chatID}).Decode(message)
 	if err != nil {
-		log.Error().Interface(kName, m.iName).Err(err).Msg("Error finding message in GetByChatId")
+		m.logger.Error().Interface(kName, m.iName).Err(err).Msg("Error finding message in GetByChatId")
 		return nil, err
 	}
 	return message, nil
@@ -80,14 +82,14 @@ func (m messageRepository) GetBySenderID(ctx context.Context, userId string) (*m
 	// sender user ID to search for
 	senderID, err := primitive.ObjectIDFromHex(userId)
 	if err != nil {
-		log.Error().Interface(kName, m.iName).Err(err).Msg("failed to convert id to object id")
-		log.Debug().Interface(kName, m.iName).Err(err).Msg("failed to convert id:" + userId)
+		m.logger.Error().Interface(kName, m.iName).Err(err).Msg("failed to convert id to object id")
+		m.logger.Debug().Interface(kName, m.iName).Err(err).Msg("failed to convert id:" + userId)
 		return nil, err
 	}
 	message := &models.Message{}
 	err = m.Collection.FindOne(ctx, bson.M{"senderId": senderID}).Decode(message)
 	if err != nil {
-		log.Error().Interface(kName, m.iName).Err(err).Msg("Error finding message in GetBySenderId")
+		m.logger.Error().Interface(kName, m.iName).Err(err).Msg("Error finding message in GetBySenderId")
 		return nil, err
 	}
 	return message, nil
@@ -108,7 +110,7 @@ func (m messageRepository) List(ctx context.Context, page, limit int) ([]models.
 	// Execute the find operation with options
 	cursor, err := m.Collection.Find(ctx, bson.M{}, findOptions)
 	if err != nil {
-		log.Error().Interface(kName, m.iName).Err(err).Msg("failed to query settings")
+		m.logger.Error().Interface(kName, m.iName).Err(err).Msg("failed to query settings")
 		return nil, err
 	}
 
@@ -116,14 +118,14 @@ func (m messageRepository) List(ctx context.Context, page, limit int) ([]models.
 	defer func(cursor *mongo.Cursor, ctx context.Context) {
 		err := cursor.Close(ctx)
 		if err != nil {
-			log.Error().Interface(kName, m.iName).Err(err).Msg("failed to close cursor in messageRepository.List")
+			m.logger.Error().Interface(kName, m.iName).Err(err).Msg("failed to close cursor in messageRepository.List")
 		}
 	}(cursor, ctx)
 
 	// Parse all the documents
 	var messages []models.Message
 	if err := cursor.All(ctx, &messages); err != nil {
-		log.Error().Interface(kName, m.iName).Err(err).Msg("failed to decode messages")
+		m.logger.Error().Interface(kName, m.iName).Err(err).Msg("failed to decode messages")
 		return nil, err
 	}
 	return messages, nil
@@ -135,7 +137,7 @@ func (m messageRepository) Update(ctx context.Context, message *models.Message) 
 
 	_, err := m.Collection.UpdateOne(ctx, bson.M{"id": message.ID}, bson.M{"$set": message})
 	if err != nil {
-		log.Error().Interface(kName, m.iName).Err(err).Msg("failed to update message with id: " + message.ID.String())
+		m.logger.Error().Interface(kName, m.iName).Err(err).Msg("failed to update message with id: " + message.ID.String())
 		return err
 	}
 	return nil
@@ -147,13 +149,13 @@ func (m messageRepository) Delete(ctx context.Context, id string) error {
 	// message ID to search for
 	messageID, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
-		log.Error().Interface(kName, m.iName).Err(err).Msg("failed to convert id to object id")
-		log.Debug().Interface(kName, m.iName).Err(err).Msg("failed to convert id:" + id)
+		m.logger.Error().Interface(kName, m.iName).Err(err).Msg("failed to convert id to object id")
+		m.logger.Debug().Interface(kName, m.iName).Err(err).Msg("failed to convert id:" + id)
 		return err
 	}
 	_, err = m.Collection.DeleteOne(ctx, bson.M{"_id": messageID})
 	if err != nil {
-		log.Error().Interface(kName, m.iName).Err(err).Msg("failed to delete message with id: " + id)
+		m.logger.Error().Interface(kName, m.iName).Err(err).Msg("failed to delete message with id: " + id)
 		return err
 	}
 	return nil
