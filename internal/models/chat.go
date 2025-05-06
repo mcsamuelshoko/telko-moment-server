4f74a2e17f31e4f86d3bd91e906bc46d9f35fda1
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-05-06 08:06:06 +0200

    update: added constants & improved properties

--- internal/models/chat.go
+++ internal/models/chat.go
@@ -1,13 +1,25 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+// Defined Chat.Type constants
+// for the Chat Model
+const (
+	ChatTypeDirect  = "direct"
+	ChatTypeGroup   = "group"
+	ChatTypeChannel = "channel"
+	ChatTypeForum   = "forum"
+)
 
 type Chat struct {
-	Id            primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
-	Participants  []primitive.ObjectID `json:"participants" bson:"participants"`
-	ChatType      string               `json:"chatType" bson:"chatType"`
-	ChatName      string               `json:"chatName,omitempty" bson:"chatName,omitempty"`
-	CreatedAt     primitive.DateTime   `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
-	UpdatedAt     primitive.DateTime   `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
-	LastMessageId primitive.ObjectID   `json:"lastMessageId,omitempty" bson:"lastMessageId,omitempty"`
+	ID            primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	MemberCount   int64              `json:"memberCount,omitempty" bson:"memberCount,omitempty"`
+	Type          string             `json:"type" bson:"type"`
+	Name          string             `json:"name,omitempty" bson:"name,omitempty"`
+	CreatedAt     time.Time          `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
+	UpdatedAt     time.Time          `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
+	LastMessageID primitive.ObjectID `json:"lastMessageId,omitempty" bson:"lastMessageId,omitempty"`
 }
