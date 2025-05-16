35c4e4341777ec36970e794757d9a3f87ecdd92a
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-05-16 22:37:33 +0200

    update message.go

--- internal/models/message.go
+++ internal/models/message.go
@@ -5,6 +5,11 @@ import (
 	"time"
 )
 
+// Define Message-Status constants
+const (
+// MessageStatus
+)
+
 type Message struct {
 	ID                 primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
 	ChatID             primitive.ObjectID   `json:"chatId" bson:"chatId"`
@@ -21,3 +26,23 @@ type Message struct {
 	CreatedAt          time.Time            `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
 	UpdatedAt          time.Time            `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
 }
+
+//// CreateUniqueIndexes creates unique indexes for username, phoneNumber and email
+//func (m *Message) CreateUniqueIndexes(db *mongo.Database) error {
+//	// Create unique index for username-hash & email+phone-hash
+//	usernameHashIndex := mongo.IndexModel{
+//		Keys:    bson.D{{"usernameHash", 1}},
+//		Options: options.Index().SetUnique(true).SetName("unique_username_hash"),
+//	}
+//
+//	emailAndPhoneNumberHashIndex := mongo.IndexModel{
+//		Keys:    bson.D{{"emailHash", 1}, {"phoneNumberHash", 1}},
+//		Options: options.Index().SetUnique(true).SetName("unique_email_and_phone_number_hash"),
+//	}
+//
+//	// Create indexes
+//	_, err := db.Collection("users").Indexes().
+//		CreateMany(context.Background(), []mongo.IndexModel{usernameHashIndex, emailAndPhoneNumberHashIndex})
+//
+//	return err
+//}
